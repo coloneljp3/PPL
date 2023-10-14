2cd62479d460a5c385849ffcb6753ec75e1d3375
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2023-10-13 23:07:44 -0500

    Create login.js

---
+++ functions/login.js
@@ -0,0 +1,30 @@
+'use strict'
+const mysql = require('mysql2');
+const express = require('express');
+const app = express();
+var bodyParser = require('body-parser');
+const serverless = require('serverless-http');
+const router = express.Router();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+router.post('/',(req,res)=>{
+var user = req.body.user;
+var email_user = req.body.email_user;
+var email_psw = req.body.email_psw;
+if(user == "undefined"){
+res.send("")
+  
+}
+  else{
+res.send("")
+    
+  }
+})
+
+router.get('/',(req,res)=>{
+
+  
+})
+app.use("/.netlify/functions/login",router)
+module.exports.handler = serverless(app)
